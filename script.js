12f7ee038a5c8fa8c30fe7937d4c38242f28c148
nicopressez <154272808+nicopressez@users.noreply.github.com>
2023-12-25 16:16:36 +0100

    Added buttons variable, function to edit display
    Added all selectors for each button to JS.
    Basic function to be triggered when a button is pressed, and update the display to add the number pressed.

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ let firstNumber = 5;
 let secondNumber = 2;
 let operator = '-' ;
 
+const zeroBtn = document.getElementById("number_zero");
+const oneBtn = document.getElementById("number_one");
+const twoBtn = document.getElementById("number_two");
+const threeBtn = document.getElementById("number_three");
+const fourBtn = document.getElementById("number_four");
+const fiveBtn = document.getElementById("number_five");
+const sixBtn = document.getElementById("number_six");
+const sevenBtn = document.getElementById("number_seven");
+const eightBtn = document.getElementById("number_eight");
+const nineBtn = document.getElementById("number_nine");
+
+const divBtn = document.getElementById("div");
+const multiplyBtn = document.getElementById("multiplier");
+const subBtn = document.getElementById("minus");
+const plusBtn = document.getElementById("plus");
+const decimalBtn = document.getElementById("decimal");
+const equalBtn = document.getElementById("equal");
+
+const currentDisplay = document.getElementById("currentNumber");
+const previousDisplay = document.getElementById("previousNumber")
+
+
 operate(operator,firstNumber,secondNumber)
 
 function add(first,second){return first + second;}
@@ -10,13 +32,27 @@ function multiply(first,second){return first * second;}
 function divide(first,second){return first / second;}
 
 function operate(operator,firstNumber,secondNumber)
-{
- if (operator == '+') 
+{if (operator == '+') 
  {console.log(add(firstNumber,secondNumber))}
  else if (operator == '-') 
  {console.log(subtract(firstNumber,secondNumber))}
  else if (operator == 'x')
  {console.log(multiply(firstNumber,secondNumber))}
  else if (operator == '/')
- {console.log(divide(firstNumber,secondNumber))};
+ {console.log(divide(firstNumber,secondNumber))};}
+
+
+// when I click on X button, return X.value  on display
+function updateDisplay(button)
+{
+    let numberPressed =  buttonsArray.find(item => item.buttons == button).value;
+    currentDisplay.textContent = numberPressed;
 }
+
+// Todo: add all numbers to the array
+// Todo: add event listeners to every button to trigger the updateDisplay function
+// Todo: store display value in a variable
+
+buttonsArray = [
+    {buttons: "zero", value: 0}
+]
